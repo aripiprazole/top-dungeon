daee958e392efaeb4e11ab3ace68361d2ec113f4
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-12-30 03:56:56 -0300

    Refactor code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,64 +1,62 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
+    private BoxCollider2D _boxCollider;
+    private Vector3 _moveDelta;
+    private RaycastHit2D _hit;
 
     // Start is called before the first frame update
     private void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        var transformLocal = transform;
+        
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
 
-        moveDelta = new Vector3(x, y, 0);
+        _moveDelta = new Vector3(x, y, 0);
 
-        // Swap the sprite direction
-        if (moveDelta.x > 0)
+        transformLocal.localScale = _moveDelta.x switch
         {
-            transform.localScale = Vector3.one;
-        }
-        else if (moveDelta.x < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+            // Swap the sprite direction
+            > 0 => Vector3.one,
+            < 0 => new Vector3(-1, 1, 1),
+            _ => transformLocal.localScale
+        };
 
         // Check if can move by casting a box in the direction that the actor wants to move
         // If the box collider is null, it means that the way is clean and open to move in
-        hit = Physics2D.BoxCast(
-            transform.position,
-            boxCollider.size,
+        _hit = Physics2D.BoxCast(
+            transformLocal.position,
+            _boxCollider.size,
             0,
-            new Vector2(moveDelta.x, 0 ),
-            Math.Abs(moveDelta.x * Time.deltaTime),
+            new Vector2(_moveDelta.x, 0 ),
+            Math.Abs(_moveDelta.x * Time.deltaTime),
             LayerMask.GetMask("Actor", "Blocking")
         );
-        if (hit.collider == null)
+        if (_hit.collider == null)
         {
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+            transformLocal.Translate(_moveDelta.x * Time.deltaTime, 0, 0);
         }
         
-        hit = Physics2D.BoxCast(
-            transform.position,
-            boxCollider.size,
+        _hit = Physics2D.BoxCast(
+            transformLocal.position,
+            _boxCollider.size,
             0,
-            new Vector2(0, moveDelta.y),
-            Math.Abs(moveDelta.y * Time.deltaTime),
+            new Vector2(0, _moveDelta.y),
+            Math.Abs(_moveDelta.y * Time.deltaTime),
             LayerMask.GetMask("Actor", "Blocking")
         );
-        if (hit.collider == null)
+        if (_hit.collider == null)
         {
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+            transformLocal.Translate(0, _moveDelta.y * Time.deltaTime, 0);
         }
     }
 }
