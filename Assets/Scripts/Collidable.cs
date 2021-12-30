dd3ea97d7db1d634d01c0b6a2bbee52472f91cfb
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-12-30 04:20:09 -0300

    Add Collidable script

---
+++ Assets/Scripts/Collidable.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Collidable : MonoBehaviour
+{
+    public ContactFilter2D filter;
+    public BoxCollider2D boxCollider;
+    private readonly Collider2D[] hits = new Collider2D[10];
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void Update()
+    {
+        // Collision work
+        boxCollider.OverlapCollider(filter, hits);
+        for (var i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null) continue;
+            
+            OnCollide(hits[i]);
+
+            hits[i] = null;
+        }
+    }
+
+    protected virtual void OnCollide(Collider2D collidedObject)
+    {
+        Debug.Log(collidedObject.name);
+    }
+}
