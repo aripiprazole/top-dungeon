bf3f98f7a2f6b8ec792aae736d51ea376957f101
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-12-31 01:50:20 -0300

    Create mover abstraction over Player

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public abstract class Mover : Fighter
+{
+    protected BoxCollider2D BoxCollider;
+    protected Vector3 MoveDelta;
+    protected RaycastHit2D Hit;
+    protected float XSpeed = 0.75f;
+    protected float YSpeed = 0.75f;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        BoxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void UpdateMotor(Vector3 input)
+    {
+        var moveDelta = new Vector3(input.x * XSpeed, input.y * YSpeed, 0);
+        var transformLocal = transform;
+        
+        transformLocal.localScale = moveDelta.x switch
+        {
+            // Swap the sprite direction
+            > 0 => Vector3.one,
+            < 0 => new Vector3(-1, 1, 1),
+            _ => transformLocal.localScale
+        };
+
+        // Check if can move by casting a box in the direction that the actor wants to move
+        // If the box collider is null, it means that the way is clean and open to move in
+        Hit = Physics2D.BoxCast(
+            transformLocal.position,
+            BoxCollider.size,
+            0,
+            new Vector2(moveDelta.x, 0),
+            Math.Abs(moveDelta.x * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+        if (Hit.collider == null) transformLocal.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+
+        Hit = Physics2D.BoxCast(
+            transformLocal.position,
+            BoxCollider.size,
+            0,
+            new Vector2(0, moveDelta.y),
+            Math.Abs(moveDelta.y * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+        if (Hit.collider == null)
+            transformLocal.Translate(0, moveDelta.y * Time.deltaTime, 0);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,62 +1,12 @@
-using System;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
-public class Player : Fighter
+public class Player : Mover
 {
-    private BoxCollider2D _boxCollider;
-    private Vector3 _moveDelta;
-    private RaycastHit2D _hit;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        _boxCollider = GetComponent<BoxCollider2D>();
-    }
-
     private void FixedUpdate()
     {
-        var transformLocal = transform;
-        
         var x = Input.GetAxisRaw("Horizontal");
         var y = Input.GetAxisRaw("Vertical");
 
-        _moveDelta = new Vector3(x, y, 0);
-
-        transformLocal.localScale = _moveDelta.x switch
-        {
-            // Swap the sprite direction
-            > 0 => Vector3.one,
-            < 0 => new Vector3(-1, 1, 1),
-            _ => transformLocal.localScale
-        };
-
-        // Check if can move by casting a box in the direction that the actor wants to move
-        // If the box collider is null, it means that the way is clean and open to move in
-        _hit = Physics2D.BoxCast(
-            transformLocal.position,
-            _boxCollider.size,
-            0,
-            new Vector2(_moveDelta.x, 0 ),
-            Math.Abs(_moveDelta.x * Time.deltaTime),
-            LayerMask.GetMask("Actor", "Blocking")
-        );
-        if (_hit.collider == null)
-        {
-            transformLocal.Translate(_moveDelta.x * Time.deltaTime, 0, 0);
-        }
-        
-        _hit = Physics2D.BoxCast(
-            transformLocal.position,
-            _boxCollider.size,
-            0,
-            new Vector2(0, _moveDelta.y),
-            Math.Abs(_moveDelta.y * Time.deltaTime),
-            LayerMask.GetMask("Actor", "Blocking")
-        );
-        if (_hit.collider == null)
-        {
-            transformLocal.Translate(0, _moveDelta.y * Time.deltaTime, 0);
-        }
+        UpdateMotor(new Vector3(x, y, 0));
     }
 }
